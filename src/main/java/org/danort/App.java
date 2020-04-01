938bdfb67ab330b45c1908ae227e00c4214d3686
Dan Ortega <ortega.dan2010@gmail.com>
2020-03-31 22:32:53 -0500

    Adding files

--- src/main/java/org/danort/App.java
+++ src/main/java/org/danort/App.java
@@ -1,19 +1,73 @@
 package org.danort;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
+import org.opencv.face.EigenFaceRecognizer;
+import org.opencv.imgcodecs.Imgcodecs;
 
 /**
  * Hello world!
  *
  */
 public class App {
+
+    public static String csvFilePath = "\\src\\resources\\FaceRecognition\\TrainingData.txt";
+
     public static void main(String[] args) {
 
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-        System.out.println("mat = " + mat.dump());
-        
+        ArrayList<Mat> images = new ArrayList<>();
+        ArrayList<Integer> labels = new ArrayList<>();
+        readCSV(csvFilePath, images, labels);
+
+        Mat testSample = images.get(images.size() - 1);
+        Integer testLabel = labels.get(images.size() - 1);
+        images.remove(images.size() - 1);
+        labels.remove(labels.size() - 1);
+        MatOfInt labelsMat = new MatOfInt();
+        labelsMat.fromList(labels);
+        EigenFaceRecognizer efr = EigenFaceRecognizer.create();
+        System.out.println("Starting training...");
+        efr.train(images, labelsMat);
+
+        int[] outLabel = new int[1];
+        double[] outConf = new double[1];
+        System.out.println("Starting Prediction...");
+        efr.predict(testSample, outLabel, outConf);
+
+        System.out.println("***Predicted label is " + outLabel[0] + ".***");
+
+        System.out.println("***Actual label is " + testLabel + ".***");
+        System.out.println("***Confidence value is " + outConf[0] + ".***");
+
+    }
+
+    private static void readCSV(String csvFilePath2, ArrayList<Mat> images, ArrayList<Integer> labels) {
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new FileReader(csvFilePath2));
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] tokens = line.split("\\;");
+                Mat readImage = Imgcodecs.imread(tokens[0], 0);
+                images.add(readImage);
+                labels.add(Integer.parseInt(tokens[1]));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
